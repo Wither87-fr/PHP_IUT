8af4522796d8ed4f42b8eb0e83a805befe704f38
__Wither <wither.plugins@gmail.com>
2020-11-06 17:12:50 +0100

    purposse

--- classes/Propose.class.php
+++ classes/Propose.class.php
@@ -1,4 +1,94 @@
 <?php
-class Propose{
-	//A COMPLETER
-}
+  /**
+   *
+   */
+  class Propose
+  {
+    private $parcoursNum;
+    private $personneNum;
+    private $date;
+    private $heure;
+    private $places;
+    private $sens;
+
+    public function __construct($valeurs)
+    {
+      if(!empty($valeurs)) {
+        $this->affecte($valeurs);
+      }
+    }
+
+
+
+
+    public function affecte($donnees) {
+      foreach ($donees as $key => $value) {
+        switch ($key) {
+          case 'par_num':
+            $this->setParcoursNum($value);
+            break;
+          case 'per_num':
+            $this->setPersonneNum($value);
+            break;
+          case 'pro_date':
+            $this->setDate($value);
+            break;
+          case 'pro_time':
+            $this->setHeure($value);
+            break;
+          case 'pro_place':
+            $this->setPlaces($value);
+            break;
+          case 'pro_sens':
+            $this->setSens($value);
+            break;
+
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+
+
+  public function setParcoursNum($num) {
+    $this->parcoursNum = $num;
+  }
+  public function setPersonneNum($num) {
+    $this->personneNum = $num;
+  }
+  public function setDate($date) {
+    $this->parcoursDate = $date;
+  }
+  public function setHeure($heure) {
+    $this->heure = $heure;
+  }
+  public function setPlaces($nb) {
+    $this->places = $nb;
+  }
+  public function setSens($sens) {
+    $this->sens = $sens;
+  }
+
+
+  public function getParcoursNum() {
+    return $this->parcoursNum;
+  }
+  public function getPersonneNum() {
+    return $this->personneNum;
+  }
+  public function getDate() {
+    return $this->parcoursDate;
+  }
+  public function getHeure() {
+    return $this->heure;
+  }
+  public function getPlaces() {
+    return $this->place;
+  }
+  public function getSens() {
+    return $this->sens;
+  }
+
+?>
--- classes/ProposeManager.class.php
+++ classes/ProposeManager.class.php
@@ -1,6 +1,32 @@
 <?php
-class ProposeManager{
-	//A COMPLETER
-	
+/**
+ *
+ */
+class ProposeManager
+{
+	private $db;
+	public function __construct($db)
+	{
+		$this->db = $db;
+	}
 
+	public function propose($par_num, $per_num, $date, $heure, $places, $sens) {
+		$sql = "INSERT INTO propose(par_num, per_num, pro_date, pro_time, pro_place, pro_sens) VALUES (:par_num, :per_num, :dateProp, :heure, :places, :sens)";
+		$req = $this->db->prepare($sql);
+		$req->bindValue(':par_num', $par_num);
+		$req->bindValue(':per_num', $per_num);
+		$req->bindValue(':dateProp', $date, PDO::PARAM_STR);
+		$req->bindValue(':heure', $heure, PDO::PARAM_STR);
+		$req->bindValue(':places', $places);
+		$req->bindValue(':sens', $sens);
+
+		$effectue = $req->execute();
+		$req->closeCursor();
+		return $effectue;
+	}
+
+	public function getPropositions($propose) {
+		$sql = "SELECT p.vil_num1, p.vil_num2, pr.pro_date, pr.pro_time, pr.pro_place, pe.per_nom FROM parcours p, propose pr, personne pe WHERE p.par_num = pr.par_num AND pr.per_num = pe.per_num AND pr.par_num = :par_num AND pr.pro_date = :pro_date AND pr.pro_time = :pro_time"
+		//propose a revoir pour tout stocker
+	}
 }
